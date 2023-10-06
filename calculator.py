3833c31a6c99b8fa3155f8baaaf8584e7a57bb43
juvisingh <141285456+juvisingh@users.noreply.github.com>
2023-10-05 22:09:02 -0700

    finished calculator

--- calculator.py
+++ calculator.py
@@ -18,65 +18,143 @@ def Mod (num1, num2):
     value5 = float(num1) % float(num2)
     return str(int(value5))
 whileVariable = True
+answer = ""
 while whileVariable == True:
     outputWanted = input("What function would you like to preform? ")
     outputWanted = outputWanted.lower()
     if (outputWanted.capitalize() == "Addition" ):
-        num1 = input("What is the first number (of the two) you would like to add? ")
+        if (answer == ""):
+            num1 = input("What is the first number (of the two) you would like to add? ")
+        else:
+            num1 = input("What is the first number (of the two) you would like to add (If you would like to use the previous answer, leave this blank)? ")
+        if (num1 == ""):
+            num1 = answer
         num2 = input("What is the second number (of the two) you would like to add? ")
-        print(num1 + " + " + num2 + " = " + Addition(num1, num2))
+        answer = Addition(num1, num2)
+        print(str(num1) + " + " + str(num2) + " = " + Addition(num1, num2))
 
     elif (outputWanted.capitalize() == "Subtraction" ):
-        num1 = input("What is the first number (of the two) you would like to subtract? ")
+        if (answer == ""):
+            num1 = input("What is the first number (of the two) you would like to subtract? ")
+        else:
+            num1 = input("What is the first number (of the two) you would like to subtract (If you would like to use the previous answer, leave this blank)? ")
+        if (num1 == ""):
+            num1 = answer
         num2 = input("What is the second number (of the two) you would like to subtract? ")
-        print(num1 + " - " + num2 + " = " + Subtraction(num1, num2))
+        answer = Subtraction(num1, num2)
+        print(str(num1) + " - " + str(num2) + " = " + Subtraction(num1, num2))
 
     elif (outputWanted.capitalize() == "Multiplication" ):
-        num1 = input("What is the first number (of the two) you would like to multiply? ")
+        if (answer == ""):
+            num1 = input("What is the first number (of the two) you would like to multiply? ")
+        else:
+            num1 = input("What is the first number (of the two) you would like to multiply (If you would like to use the previous answer, leave this blank)? ")
+        if (num1 == ""):
+            num1 = answer
         num2 = input("What is the second number (of the two) you would like to multiply? ")
-        print(num1 + " x " + num2 + " = " + Multiplication(num1, num2))
+        answer = Multiplication(num1, num2)
+        print(str(num1) + " x " + str(num2) + " = " + Multiplication(num1, num2))
+    
+    elif (outputWanted.capitalize() == "Division" ):
+        if (answer == ""):
+            num1 = input("What is the first number (of the two) you would like to divide? ")
+        else:
+            num1 = input("What is the first number (of the two) you would like to divide (If you would like to use the previous answer, leave this blank)? ")
+        if (num1 == ""):
+            num1 = answer
+        num2 = input("What is the second number (of the two) you would like to divide? ")
+        answer = Division(num1, num2)
+        print(str(num1) + " / " + str(num2) + " = " + Division(num1, num2))
 
     elif (outputWanted.capitalize() == "Exponent" ):
-        num1 = input("What is the base? ")
+        if (answer == ""):
+            num1 = input("What is the base? ")
+        else:
+            num1 = input("What is the base? (If you would like to use the previous answer, leave this blank)? ")
+        if (num1 == ""):
+            num1 = answer
         num2 = input("What is the exponent? ")
-        print(num1 + " ^ " + num2 + " = " + Exponent(num1, num2))
+        answer = Exponent(num1, num2)
+        print(str(num1) + " ^ " + str(num2) + " = " + Exponent(num1, num2))
 
     elif (outputWanted.capitalize() == "Modulus" ):
-        num1 = input("What is the first number (of the two) you would like to find the remainder of? ")
+        if (answer == ""):
+            num1 = input("What is the first number (of the two) you would like to find the remainder of?")
+        else:
+            num1 = input("What is the first number (of the two) you would like to find the remainder of? (If you would like to use the previous answer, leave this blank)? ")
+        if (num1 == ""):
+            num1 = answer
         num2 = input("What is the second number (of the two) you would like to find the remainder of? ")
-        print(num1 + " % " + num2 + " = " + Mod(num1, num2) + "R") 
+        answer = Mod(num1, num2)
+        print(str(num1) + " % " + str(num2) + " = " + Mod(num1, num2) + "R") 
 
     elif (outputWanted.capitalize() == "Square root" ):
-        num1 = input("What is number would you like to find the sqaure root of? ")
-        num1 = int(num1)
+        if (answer == ""):
+            num1 = input("What number would you like to find the sqaure root of? ")
+        else:
+            num1 = input("What number would you like to find the sqaure root of? (If you would like to use the previous answer, leave this blank)? ")
+        if (num1 == ""):
+            num1 = float(answer)
+        num1 = float(num1)
+        answer = math.sqrt(num1)
         print("√" + str(num1) + " = " + str(math.sqrt(num1))) 
     elif (outputWanted.capitalize() == "Log" ):
-        num1 = input("What is the number you would like to Log? ")
-        num1 = int(num1)
+        if (answer == ""):
+            num1 = input("What number would you like to log? ")
+        else:
+            num1 = input("What number would you like to log? (If you would like to use the previous answer, leave this blank)? ")
+        if (num1 == ""):
+            num1 = float(answer)
+        num1 = float(num1)
         num2 = input("What is the base (optional)? ")
         if (str(num2) == ""):
             num2 = 10
-        num2 = int(num2)
+        num2 = float(num2)
+        answer = math.log(num1, num2)
         print("log(" + str(num1) + ")" + " = " + str(math.log(num1, num2))) 
 
     elif (outputWanted.capitalize() == "Natural log"):
-        num1 = input("What is the number you would like to find the natural log of? ")
-        num1 = int(num1)
+        if (answer == ""):
+            num1 = input("What number would you like to find the natural log of? ")
+        else:
+            num1 = input("What number would you like to find the natural log of? (If you would like to use the previous answer, leave this blank)? ")
+        if (num1 == ""):
+            num1 = float(answer)
+        num1 = float(num1)
+        answer = math.log(num1)
         print("ln(" + str(num1) + ")" + " = " + str(math.log(num1))) 
     
     elif (outputWanted.capitalize() == "Sin"):
-        num1 = input("What number would you like to find the sin of? ")
-        num1 = int(num1)
+        if (answer == ""):
+            num1 = input("What number would you like to find the sin? ")
+        else:
+            num1 = input("What number would you like to find the sin? (If you would like to use the previous answer, leave this blank)? ")
+        if (num1 == ""):
+            num1 = float(answer)
+        num1 = float(num1)
+        answer = math.sin(num1)
         print("sin(" + str(num1) + ")" + " = " + str(math.sin(num1)))
 
     elif (outputWanted.capitalize() == "Tan"):
-        num1 = input("What number would you like to find the tan of ")
-        num1 = int(num1)
+        if (answer == ""):
+            num1 = input("What number would you like to find the tan? ")
+        else:
+            num1 = input("What number would you like to find the tan? (If you would like to use the previous answer, leave this blank)? ")
+        if (num1 == ""):
+            num1 = float(answer)
+        num1 = float(num1)
+        answer = math.tan(num1)
         print("tan(" + str(num1) + ")" + " = " + str(math.tan(num1)))
 
     elif (outputWanted.capitalize() == "Cos"):
-        num1 = input("What number would to find the cos of? ")
-        num1 = int(num1)
+        if (answer == ""):
+            num1 = input("What number would you like to find the cos? ")
+        else:
+            num1 = input("What number would you like to find the cos? (If you would like to use the previous answer, leave this blank)? ")
+        if (num1 == ""):
+            num1 = float(answer)
+        num1 = float(num1)
+        answer = math.cos(num1)
         print("cos(" + str(num1) + ")" + " = " + str(math.cos(num1))) 
     
     #I do not need this because my variables reset every time the user preforms another function. I still added it though.
@@ -84,4 +162,8 @@ while whileVariable == True:
         num1 = 0
         num2 = 0
         print("Cleared")
+    #Also, I did not add paranthesis cause I do not need it the way my calculator is set up :)
+    elif (outputWanted.capitalize() == "Exit"):
+        print("Thank you for using this calculator!")
+        break
 
